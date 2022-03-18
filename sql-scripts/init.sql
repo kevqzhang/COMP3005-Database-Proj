d4602e73c5d6f177e211184f83a947e4d7625d49
kevqzhang <kcrafterzh01@gmail.com>
2022-03-17 21:04:02 -0400

    added weapons to db

--- sql-scripts/init.sql
+++ sql-scripts/init.sql
@@ -1,18 +1,112 @@
-CREATE TABLE characters(
-    cid integer primary key autoincrement not null,
-    name text not null,
-    class text not null,
-    subclass text not null,
-    race text not null,
-    alignment text not null,
-    charLvl integer not null,
-    str integer not null,
-    dex integer not null,
-    con integer not null,
-    int integer not null,
-    wis integer not null,
-    cha integer not null,
-    maxHp integer not null,
-    wears text not null,
-    foreign key (wears) references armor(armorName)
+insert into weapons values (
+    'Light Hammer',
+    'Simple Melee',
+    '1d4',
+    'bludgeoning',
+    2,
+    'Light, thrown (range 20/60)'
+);
+insert into weapons values (
+    'Quarter Staff',
+    'Simple Melee',
+    '1d6',
+    'bludgeoning',
+    4,
+    'Versatile (1d8)'
+);
+insert into weapons values (
+    'Spear',
+    'Simple Melee',
+    '1d6',
+    'piercing',
+    3,
+    'Thrown (range 20/60), versatile (1d8)'
+);
+insert into weapons values (
+    'Crossbow',
+    'Simple Ranged',
+    '1d8',
+    'piercing',
+    5,
+    'Ammunition (range 80/320), loading, two-handed'
+);
+insert into weapons values (
+    'Shortbow',
+    'Simple Ranged',
+    '1d6',
+    'piercing',
+    2,
+    'Ammunition (range 80/320), two-handed'
+);
+insert into weapons values (
+    'Battleaxe',
+    'Martial Melee',
+    '1d8',
+    'slashing',
+    4,
+    'Versatile (1d10)'
+);
+insert into weapons values (
+    'Greatsword',
+    'Martial Melee',
+    '2d6',
+    'slashing',
+    6,
+    'Heavy, two-handed'
+);
+insert into weapons values (
+    'Longsword',
+    'Martial Melee',
+    '1d8',
+    'slashing',
+    3,
+    'Versatile (1d10)'
+);
+insert into weapons values (
+    'Pike',
+    'Martial Melee',
+    '1d10',
+    'piercing',
+    4,
+    'Heavy, reach, two-handed'
+);
+insert into weapons values (
+    'Rapier',
+    'Martial Melee',
+    '1d8',
+    'piercing',
+    2,
+    'Finesse'
+);
+insert into weapons values (
+    'Scimitar',
+    'Martial Melee',
+    '1d6',
+    'slashing',
+    3,
+    'Finesse, light'
+);
+insert into weapons values (
+    'Trident',
+    'Martial Melee',
+    '1d6',
+    'piercing',
+    4,
+    'Thrown (range 20/60), versatile (1d8)'
+);
+insert into weapons values (
+    'Warhammer',
+    'Martial Melee',
+    '1d8',
+    'bludgeoning',
+    2,
+    'Versatile (1d10)'
+);
+insert into weapons values (
+    'Longbow',
+    'Martial Ranged',
+    '1d8',
+    'piercing',
+    2,
+    'Ammunition (range 150/600), heavy, two-handed'
 );
