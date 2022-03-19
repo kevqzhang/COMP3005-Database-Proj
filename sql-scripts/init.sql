2c03f47845495d478dc457df64c31fd8c7dcfd5c
kevqzhang <kcrafterzh01@gmail.com>
2022-03-19 18:28:24 -0400

    added cantrips

--- sql-scripts/init.sql
+++ sql-scripts/init.sql
@@ -1,112 +1,72 @@
-insert into weapons values (
-    'Light Hammer',
-    'Simple Melee',
-    '1d4',
-    'bludgeoning',
-    2,
-    'Light, thrown (range 20/60)'
+insert into spells values (
+    'Thaumaturgy',
+    'Cantrip',
+    'Transmutation',
+    '30 ft',
+    '1 action',
+    'V'
 );
-insert into weapons values (
-    'Quarter Staff',
-    'Simple Melee',
-    '1d6',
-    'bludgeoning',
-    4,
-    'Versatile (1d8)'
+insert into spells values (
+    'Frostbite',
+    'Cantrip',
+    'Evocation',
+    '60 ft',
+    '1 action',
+    'V, S'
 );
-insert into weapons values (
-    'Spear',
-    'Simple Melee',
-    '1d6',
-    'piercing',
-    3,
-    'Thrown (range 20/60), versatile (1d8)'
+insert into spells values (
+    'Friends',
+    'Cantrip',
+    'Enchantment',
+    'Self',
+    '1 action',
+    'S, M'
 );
-insert into weapons values (
-    'Crossbow',
-    'Simple Ranged',
-    '1d8',
-    'piercing',
-    5,
-    'Ammunition (range 80/320), loading, two-handed'
+insert into spells values (
+    'Gust',
+    'Cantrip',
+    'Transmutation',
+    '30 ft',
+    '1 action',
+    'V, S'
 );
-insert into weapons values (
-    'Shortbow',
-    'Simple Ranged',
-    '1d6',
-    'piercing',
-    2,
-    'Ammunition (range 80/320), two-handed'
+insert into spells values (
+    'Light',
+    'Cantrip',
+    'Conjuration',
+    'Touch',
+    '1 action',
+    'V, M'
 );
-insert into weapons values (
-    'Battleaxe',
-    'Martial Melee',
-    '1d8',
-    'slashing',
-    4,
-    'Versatile (1d10)'
+insert into spells values (
+    'Mending',
+    'Cantrip',
+    'Transmutation',
+    'Touch',
+    '1 action',
+    'V, S, M'
 );
-insert into weapons values (
-    'Greatsword',
-    'Martial Melee',
-    '2d6',
-    'slashing',
-    6,
-    'Heavy, two-handed'
+insert into spells values (
+    'Prestidigitation',
+    'Cantrip',
+    'Transmutation',
+    '10 ft',
+    '1 action',
+    'V, S'
 );
-insert into weapons values (
-    'Longsword',
-    'Martial Melee',
-    '1d8',
-    'slashing',
-    3,
-    'Versatile (1d10)'
+insert into spells values (
+    'Thunderclap',
+    'Cantrip',
+    'Transmutation',
+    '5 ft',
+    '1 action',
+    'S'
 );
-insert into weapons values (
-    'Pike',
-    'Martial Melee',
-    '1d10',
-    'piercing',
-    4,
-    'Heavy, reach, two-handed'
-);
-insert into weapons values (
-    'Rapier',
-    'Martial Melee',
-    '1d8',
-    'piercing',
-    2,
-    'Finesse'
-);
-insert into weapons values (
-    'Scimitar',
-    'Martial Melee',
-    '1d6',
-    'slashing',
-    3,
-    'Finesse, light'
-);
-insert into weapons values (
-    'Trident',
-    'Martial Melee',
-    '1d6',
-    'piercing',
-    4,
-    'Thrown (range 20/60), versatile (1d8)'
-);
-insert into weapons values (
-    'Warhammer',
-    'Martial Melee',
-    '1d8',
-    'bludgeoning',
-    2,
-    'Versatile (1d10)'
-);
-insert into weapons values (
-    'Longbow',
-    'Martial Ranged',
-    '1d8',
-    'piercing',
-    2,
-    'Ammunition (range 150/600), heavy, two-handed'
+insert into spells values (
+    'Resistance',
+    'Cantrip',
+    'Abjuration',
+    'Touch',
+    '1 action',
+    'V, S, M'
 );
