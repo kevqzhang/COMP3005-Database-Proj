b3fea4762d77db4be55b241555c1e754b7dcf864
kevqzhang <kcrafterzh01@gmail.com>
2022-03-21 21:09:08 -0400

    added characters to db

--- sql-scripts/init.sql
+++ sql-scripts/init.sql
@@ -1,68 +1,17 @@
-/*
-insert into characters (
-    name,
-    class,
-    subclass,
-    race,
-    alignment,
-    charLvl,
-    str,
-    dex,
-    con,
-    int,
-    wis,
-    cha,
-    maxHp,
-    wears
-) values (
-    'Tiphany',
-    'Barbarian',
-    'Ancestral Guardian',
-    'Dwarf',
-    'Neutral Good',
-    6,
-    14,
-    10,
-    16,
-    8,
-    9,
-    9,
-
-)
-*/
-
-insert into armor values (
-    'Padded',
-    'Light Armor',
-    '11 + Dex modifier',
-    8,
-    1
-);
-insert into armor values (
-    'Breastplate',
-    'Medium Armor',
-    '14 + Dex modifier(max 2)',
-    8,
-    0
-);
-insert into armor values (
-    'Hide',
-    'Medium Armor',
-    '11 + Dex modifier(max 2)',
+insert into characters (name, class, subclass, race, alignment, charLvl, str, dex, con, int, wis, cha, maxHp, wears ) 
+values (
+    'Billiam Bartlesbee',
+    'Ranger',
+    'Beast Master',
+    'Gnome',
+    'True Neutral',
+    5,
     12,
-    0
-);
-insert into armor values (
-    'Splint',
-    'Heavy Armor',
-    '17',
-    60,
-    1
-);
-insert into armor values (
-    'Plate',
-    'Heavy Armor',
-    '18',
-    65,
-    1
+    16,
+    11,
+    8,
+    12,
+    9,
+    23,
+    'Padded'
 );
