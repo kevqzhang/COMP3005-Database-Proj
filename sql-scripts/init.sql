ea1bc912690b6da2bc1197b17b19789d923b26a7
kevqzhang <kcrafterzh01@gmail.com>
2022-03-25 22:12:00 -0400

    added wields entries

--- sql-scripts/init.sql
+++ sql-scripts/init.sql
@@ -1,17 +1,138 @@
-insert into characters (name, class, subclass, race, alignment, charLvl, str, dex, con, int, wis, cha, maxHp, wears ) 
+insert into wields
 values (
-    'Billiam Bartlesbee',
-    'Ranger',
-    'Beast Master',
-    'Gnome',
-    'True Neutral',
-    5,
-    12,
-    16,
-    11,
-    8,
-    12,
-    9,
-    23,
-    'Padded'
+    1, 
+    'WarHammer',
+    1
+);
+insert into wields
+values (
+    1, 
+    'Handaxe',
+    1
+);
+insert into wields
+values (
+    3, 
+    'Longbow',
+    1
+);
+insert into wields
+values (
+    3, 
+    'Dagger',
+    1
+);
+insert into wields
+values (
+    4, 
+    'Dagger',
+    1
+);
+insert into wields
+values (
+    4, 
+    'Trident',
+    0
+);
+insert into wields
+values (
+    3, 
+    'Spear',
+    1
+);
+insert into wields
+values (
+    5, 
+    'Dagger',
+    1
+);
+insert into wields
+values (
+    5, 
+    'Crossbow',
+    1
+);
+insert into wields
+values (
+    6, 
+    'Quarterstaff',
+    1
+);
+insert into wields
+values (
+    7, 
+    'Rapier',
+    1
+);
+insert into wields
+values (
+    7, 
+    'Trident',
+    1
+);
+insert into wields
+values (
+    8, 
+    'Scimitar',
+    1
+);
+insert into wields
+values (
+    8, 
+    'Crossbow',
+    1
+);
+insert into wields
+values (
+    8, 
+    'Dagger',
+    1
+);
+insert into wields
+values (
+    8, 
+    'Trident',
+    1
+);
+insert into wields
+values (
+    9, 
+    'Longbow',
+    1
+);
+insert into wields
+values (
+    9, 
+    'Battleaxe',
+    1
+);
+insert into wields
+values (
+    9, 
+    'Greatsword',
+    1
+);
+insert into wields
+values (
+    10, 
+    'Handaxe',
+    1
+);
+insert into wields
+values (
+    10, 
+    'Longsword',
+    0
+);
+insert into wields
+values (
+    11, 
+    'Greatsword',
+    1
+);
+insert into wields
+values (
+    11, 
+    'Spear',
+    1
 );
