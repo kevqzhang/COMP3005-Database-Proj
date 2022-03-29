2332bdd5761dcedcc541eae20839b945e52f5c0c
kevqzhang <kcrafterzh01@gmail.com>
2022-03-28 21:56:44 -0400

    added bard spells and update character view

--- sql-scripts/init.sql
+++ sql-scripts/init.sql
@@ -1,138 +1,50 @@
-insert into wields
-values (
-    1, 
-    'WarHammer',
-    1
-);
-insert into wields
-values (
-    1, 
-    'Handaxe',
-    1
-);
-insert into wields
+insert into casts
 values (
     3, 
-    'Longbow',
-    1
+    'Mending'
 );
-insert into wields
+insert into casts
 values (
     3, 
-    'Dagger',
-    1
+    'Prestidigitation'
 );
-insert into wields
-values (
-    4, 
-    'Dagger',
-    1
-);
-insert into wields
-values (
-    4, 
-    'Trident',
-    0
-);
-insert into wields
+insert into casts
 values (
     3, 
-    'Spear',
-    1
+    'Friends'
 );
-insert into wields
+insert into casts
 values (
-    5, 
-    'Dagger',
-    1
+    3, 
+    'Thunderwave'
 );
-insert into wields
+insert into casts
 values (
-    5, 
-    'Crossbow',
-    1
+    3, 
+    'Sleep'
 );
-insert into wields
+insert into casts
 values (
-    6, 
-    'Quarterstaff',
-    1
+    3, 
+    'Command'
 );
-insert into wields
+insert into casts
 values (
-    7, 
-    'Rapier',
-    1
+    3, 
+    'Blindness/Deafness'
 );
-insert into wields
+insert into casts
 values (
-    7, 
-    'Trident',
-    1
+    3, 
+    'Fear'
 );
-insert into wields
+insert into casts
 values (
-    8, 
-    'Scimitar',
-    1
+    3, 
+    'Pyrotechnics'
 );
-insert into wields
+insert into casts
 values (
-    8, 
-    'Crossbow',
-    1
-);
-insert into wields
-values (
-    8, 
-    'Dagger',
-    1
-);
-insert into wields
-values (
-    8, 
-    'Trident',
-    1
-);
-insert into wields
-values (
-    9, 
-    'Longbow',
-    1
-);
-insert into wields
-values (
-    9, 
-    'Battleaxe',
-    1
-);
-insert into wields
-values (
-    9, 
-    'Greatsword',
-    1
-);
-insert into wields
-values (
-    10, 
-    'Handaxe',
-    1
-);
-insert into wields
-values (
-    10, 
-    'Longsword',
-    0
-);
-insert into wields
-values (
-    11, 
-    'Greatsword',
-    1
-);
-insert into wields
-values (
-    11, 
-    'Spear',
-    1
+    3, 
+    'Invisibility'
 );
